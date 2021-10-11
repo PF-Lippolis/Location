0668757355c83149406d3be396a590a6aad42bda
P.F. Lippolis <pf.lippolis@gmail.com>
2021-10-11 17:53:13 +0200

    Create app skeleton

--- app/src/main/java/com/example/location/MainActivity.kt
+++ app/src/main/java/com/example/location/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.example.location
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED -> {
+                    showLocation()
+                }
+                ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED -> {
+                    tryUpgradeLocationPermissions()
+                    showLocation()
+                }
+                else ->{
+                    askForPermissions()
+                }
+            }
+        }
+    }
+
+    private fun askForPermissions() {
+        TODO("Not yet implemented")
+    }
+
+    private fun tryUpgradeLocationPermissions() {
+        TODO("Not yet implemented")
+    }
+
+    private fun showLocation() {
+        
     }
 }
